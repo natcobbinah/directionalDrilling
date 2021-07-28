3510b4ae3f3bbc51da19b87f801fdfc4c1eec69f
natcob <nathaniel.cobbinah@amalitech.com>
2021-07-28 09:30:54 +0000

    compiled files in /lib linted

--- lib/directionDrilling.d.ts
+++ lib/directionDrilling.d.ts
@@ -1,2 +1,1 @@
-declare class directionalDrilling {
-}
+declare class directionalDrilling {}
--- lib/directionalDrillingMethods.d.ts
+++ lib/directionalDrillingMethods.d.ts
@@ -1,12 +1,12 @@
 export interface DirectionDrillingMethods {
-    build_UpAngle(value: number): number;
-    horizontal_Displacement(value: number, kickoffPoint: number, trueVerticalDepth: number): number;
-    total_AngleY(): number;
-    total_AngleXplusY(): number;
-    measured_DepthAtEndOfBuildSection(kickoffPoint: number): number;
-    trueVertical_DepthAtEndOfBuild(KickOffPoint: number): number;
-    horizontal_DeviationAtEndOfBuildUp(): number;
-    total_MeasuredDepth(trueVerticalDepth: number, kickoffPoint: number): number;
-    toDegrees(radians: number): number;
-    toRadians(degrees: number): number;
+  build_UpAngle(value: number): number;
+  horizontal_Displacement(value: number, kickoffPoint: number, trueVerticalDepth: number): number;
+  total_AngleY(): number;
+  total_AngleXplusY(): number;
+  measured_DepthAtEndOfBuildSection(kickoffPoint: number): number;
+  trueVertical_DepthAtEndOfBuild(KickOffPoint: number): number;
+  horizontal_DeviationAtEndOfBuildUp(): number;
+  total_MeasuredDepth(trueVerticalDepth: number, kickoffPoint: number): number;
+  toDegrees(radians: number): number;
+  toRadians(degrees: number): number;
 }
