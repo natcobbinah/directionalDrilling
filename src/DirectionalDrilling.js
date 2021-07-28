7c3bf9cb1c2f49121d26c0850ebc7d533e96e78c
natcob <nathaniel.cobbinah@amalitech.com>
2021-07-28 12:38:06 +0000

    unit Test performed on methods

---
+++ src/DirectionalDrilling.js
@@ -0,0 +1,79 @@
+"use strict";
+exports.__esModule = true;
+exports.DirectionalDrilling = void 0;
+var DirectionalDrilling = /** @class */ (function () {
+    function DirectionalDrilling() {
+        this.valueOverbuildUp = 36000;
+        this.valuetimesbuildUp = 6.283185307179586;
+        this.maxRadianValue = 180;
+        this.valuetimesTotalAngleXplusY = 2;
+        this.valueDividebyBuildUpAngle = 360;
+        this._buildUpAngle = 0;
+        this._horizontalDisplacement = 0;
+        this._kickOffPoint = 0;
+        this._trueVerticalDepth = 0;
+    }
+    DirectionalDrilling.prototype.toDegrees = function (radians) {
+        return radians * (this.maxRadianValue / Math.PI);
+    };
+    DirectionalDrilling.prototype.toRadians = function (degrees) {
+        return degrees * (Math.PI / 180);
+    };
+    DirectionalDrilling.prototype.build_UpAngle = function (value) {
+        this._buildUpAngle = value;
+        var radius = (this.valueOverbuildUp / (value * this.valuetimesbuildUp));
+        return radius;
+    };
+    DirectionalDrilling.prototype.horizontal_Displacement = function (value, kickoffPoint, trueVerticalDepth) {
+        this._horizontalDisplacement = value;
+        this._kickOffPoint = kickoffPoint;
+        this._trueVerticalDepth = trueVerticalDepth;
+        var angleX = (value - this.build_UpAngle(this._buildUpAngle)) /
+            (this._trueVerticalDepth - this._kickOffPoint);
+        return this.toDegrees(Math.atan(angleX));
+    };
+    DirectionalDrilling.prototype.total_AngleY = function () {
+        var angleY = this.build_UpAngle(this._buildUpAngle) *
+            Math.cos(this.toRadians(this.horizontal_Displacement(this._horizontalDisplacement, this._kickOffPoint, this._trueVerticalDepth)))
+            /
+                (this._trueVerticalDepth - this._kickOffPoint);
+        return this.toDegrees(Math.asin(angleY));
+    };
+    DirectionalDrilling.prototype.total_AngleXplusY = function () {
+        var sumofAngleXplusY = this.total_AngleY() +
+            this.horizontal_Displacement(this._horizontalDisplacement, this._kickOffPoint, this._trueVerticalDepth);
+        return sumofAngleXplusY;
+    };
+    DirectionalDrilling.prototype.measured_DepthAtEndOfBuildSection = function (kickoffPoint) {
+        this._kickOffPoint = kickoffPoint;
+        var setMeasuredDepth = this.total_AngleXplusY() * this.valuetimesTotalAngleXplusY * Math.PI *
+            this.build_UpAngle(this._buildUpAngle)
+            /
+                this.valueDividebyBuildUpAngle + kickoffPoint;
+        return setMeasuredDepth;
+    };
+    DirectionalDrilling.prototype.trueVertical_DepthAtEndOfBuild = function (KickOffPoint) {
+        this._kickOffPoint = KickOffPoint;
+        var trueVerticalDepthCompute = this.build_UpAngle(this._buildUpAngle) *
+            Math.sin(this.toRadians(this.total_AngleXplusY())) + KickOffPoint;
+        return trueVerticalDepthCompute;
+    };
+    DirectionalDrilling.prototype.horizontal_DeviationAtEndOfBuildUp = function () {
+        var radiusValue = this.build_UpAngle(this._buildUpAngle);
+        var cosValue = radiusValue * Math.cos(this.toRadians(this.total_AngleXplusY()));
+        return (radiusValue - cosValue);
+    };
+    DirectionalDrilling.prototype.total_MeasuredDepth = function (trueVerticalDepth, kickoffPoint) {
+        this._kickOffPoint = kickoffPoint;
+        this._trueVerticalDepth = trueVerticalDepth;
+        var totalMeasuredDepth = trueVerticalDepth - kickoffPoint;
+        var Rsina = this.build_UpAngle(this._buildUpAngle) * Math.sin(this.toRadians(this.total_AngleXplusY()));
+        var cosA = Math.cos(this.toRadians(this.total_AngleXplusY()));
+        var getTrueVerticalDepth = this.measured_DepthAtEndOfBuildSection(this._kickOffPoint);
+        var setFromArcToTarget = (totalMeasuredDepth - Rsina) / cosA;
+        var totalDepth = setFromArcToTarget + getTrueVerticalDepth;
+        return totalDepth;
+    };
+    return DirectionalDrilling;
+}());
+exports.DirectionalDrilling = DirectionalDrilling;
---
+++ src/tests/DirectionalDrilling.test.ts
@@ -0,0 +1,25 @@
+import { DirectionalDrilling } from '../index'
+
+let  ddrilling = new DirectionalDrilling();
+
+test('Radius', function () {
+  expect(ddrilling.build_UpAngle(2)).toBe(2864.7889756541163);
+});
+test('Angle X', function () {
+  expect(ddrilling.horizontal_Displacement(3000, 2000, 10000)).toBe(0.9682854378192228);
+});
+test('Angle Y', function () {
+  expect(ddrilling.total_AngleY()).toBe(20.980333796791758);
+});
+test('Angle (X+Y)', function () {
+  expect(ddrilling.total_AngleXplusY()).toBe(21.94861923461098);
+});
+test('Measured Depth At the End of Build', function () {
+  expect(ddrilling.measured_DepthAtEndOfBuildSection(2000)).toBe(3097.430961730549);
+});
+test('Horizontal Deviation at End of Build', function () {
+  expect(ddrilling.horizontal_DeviationAtEndOfBuildUp()).toBe(207.64158017340878);
+});
+test('Total Measured Depth', function () {
+  expect(ddrilling.total_MeasuredDepth(10000, 2000)).toBe(10568.12478132156);
+});
